4b5edd49a078a2aedf17ed8bfafa20fb0af7ab4e
Rafael De Luca <deluca1712@gmail.com>
2022-02-20 19:22:19 -0300

    import static

---
+++ src/aula58_import_static/Program01.java
@@ -0,0 +1,30 @@
+package aula58_import_static;
+
+
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.cos;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+			
+	System.out.println("Se importar como static pode chamar os métodos direto na classe");
+	
+	
+	double a=0.86,b=17,c=81,d=-9;
+	
+	
+	System.out.println(Math.acos(a));
+	System.out.println(Math.abs(d));
+	System.out.println(Math.pow(b, 3));
+	System.out.println(Math.sqrt(c));
+	
+	System.out.println(sqrt(c));
+	System.out.println(cos(30));
+	System.out.println(pow(3,4));
+		
+		
+	}
+
+}
